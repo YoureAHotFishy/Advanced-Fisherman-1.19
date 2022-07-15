78e301c9dbcd48a6e13afe77604020fb3dc48d17
YoureAHotFishy <crisban08@gmail.com>
2022-07-14 22:03:41 -0400

    Mod is now working (base)

---
+++ src/main/java/net/youreahotfishy/advancedfisherman/MainClass.java
@@ -0,0 +1,20 @@
+package net.youreahotfishy.advancedfisherman;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainClass implements ModInitializer {
+
+	public static final String MOD_ID = "advancedfisherman";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+
+
+
+		LOGGER.info("Mod Loaded.");
+	}
+}
